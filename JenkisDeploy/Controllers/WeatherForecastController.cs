5071277c04bb3a09b5195f712e927565103118af
betsq9 <betsq9@gmail.com>
2022-07-23 13:22:16 +0300

    ds

--- JenkisDeploy/Controllers/WeatherForecastController.cs
+++ JenkisDeploy/Controllers/WeatherForecastController.cs
@@ -30,16 +30,10 @@ namespace JenkisDeploy.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "Hiii")]
-        public IEnumerable<WeatherForecast> GetHi()
+        [HttpPost(Name = "Hello")]
+        public string GetHi()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Deplyed by jenkins";
         }
     }
 }
